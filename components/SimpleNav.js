c7d976e77a249e0f200c05beb20af13a0a5995c2
icld <ian@icldesign.com>
2021-08-31 10:55:04 -0700

    add custom errors and Update product UI

--- components/SimpleNav.js
+++ components/SimpleNav.js
@@ -39,14 +39,23 @@ const SimpleNav = () => {
                 <div className='flex ml-4 space-x-4 md:space-x-8'>
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <Link href='/'>
-                    <a className='inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900 border-b-2 active:text-yellow-300'>
+                    <a
+                      className={`inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900  active:text-yellow-300 ${
+                        router.pathname == '/'
+                          ? `text-yellow-600 border-b-2`
+                          : `hover:border-gray-300 hover:text-gray-700`
+                      } `}
+                    >
                       Blog
                     </a>
                   </Link>
                   <Link href='/shop'>
                     <a
-                      // href='/shop'
-                      className='inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:border-gray-300 hover:text-gray-700 active:text-gray-700'
+                      className={`inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900  active:text-yellow-300 ${
+                        router.pathname == '/shop'
+                          ? `text-yellow-600 border-b-2`
+                          : `hover:border-gray-300 hover:text-gray-700`
+                      } `}
                     >
                       Shop
                     </a>
---
+++ pages/500.js
@@ -0,0 +1,19 @@
+export default function Example() {
+  return (
+    <div className='bg-white'>
+      <div className='px-4 py-16 mx-auto max-w-7xl sm:py-24 sm:px-6 lg:px-8'>
+        <div className='text-center'>
+          <h2 className='text-base font-semibold tracking-wide text-red-800 uppercase'>
+            404
+          </h2>
+          <p className='mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl'>
+            Whoops!
+          </p>
+          <p className='max-w-xl mx-auto mt-5 text-xl text-gray-500'>
+            {`${"We couldn't find that page!"}`}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
