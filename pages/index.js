b9070d98d0c939e77b21994b11413388bda8d02c
icld <ian@icldesign.com>
2021-08-30 21:39:12 -0700

    l

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { client } from '../lib/sanity/client';
 import { homeQuery } from '../lib/sanity/homeQuery';
 import groq from 'groq';
+import Image from 'next/image';
+import { useNextSanityImage } from 'next-sanity-image';
 
 import urlFor from '../lib/sanity/urlFor';
 
-export default function Example({ posts }) {
+const Home = ({ posts }) => {
+  const [mappedPosts, setMappedPosts] = useState([]);
+
   return (
     <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
       <div className='relative top-0 h-full px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
@@ -22,67 +27,75 @@ export default function Example({ posts }) {
             </p>
           </div>
           <div className='grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none'>
-            {posts.map((post) => (
-              <div
-                key={post.title}
-                className='flex flex-col overflow-hidden rounded-lg shadow-lg'
-              >
-                <div className='flex-shrink-0'>
-                  <img
-                    className='object-cover w-full h-48'
-                    src={urlFor(post.mainImage)}
-                    alt={post.title}
-                  />
-                </div>
-                <div className='flex flex-col justify-between flex-1 p-6 bg-white'>
-                  <div className='flex-1'>
-                    <p className='text-sm font-medium text-indigo-600'>
-                      <a href='' className='hover:underline'>
-                        {post.categories}
-                      </a>
-                    </p>
-                    <a href={`/posts/${post.slug}`} className='block mt-2'>
-                      <p className='text-xl font-semibold text-gray-900'>
-                        {post.title}
-                      </p>
-                      <p className='mt-3 text-base text-gray-500'>
-                        {post.description}
-                      </p>
-                    </a>
+            {posts.map((post) => {
+              console.log(post);
+              console.log(urlFor(post.mainImage).url());
+              return (
+                <div
+                  key={post.title}
+                  className='flex flex-col overflow-hidden rounded-lg shadow-lg'
+                >
+                  <div className='flex-shrink-0'>
+                    {/* <Image src={post.mainImage} alt='' /> */}
+                    <img
+                      src={urlFor(post.mainImage).url()}
+                      // layout='responsive'
+                      // width='200'
+                      // height='200'
+                      className='object-cover w-full h-48'
+                      alt={post.title}
+                    />
                   </div>
-                  <div className='flex items-center mt-6'>
-                    <div className='flex-shrink-0'>
-                      <a href=''>
-                        <span className='sr-only'>{post.author}</span>
-                        <img
-                          className='w-10 h-10 rounded-full'
-                          src={urlFor(post.authImg)}
-                          alt=''
-                        />
-                      </a>
-                    </div>
-                    <div className='ml-3'>
-                      <p className='text-sm font-medium text-gray-900'>
+                  <div className='flex flex-col justify-between flex-1 p-6 bg-white'>
+                    <div className='flex-1'>
+                      <p className='text-sm font-medium text-indigo-600'>
                         <a href='' className='hover:underline'>
-                          {post.authName}
+                          {post.categories}
                         </a>
                       </p>
-                      <div className='flex space-x-1 text-sm text-gray-500'>
-                        <time dateTime={post.datetime}>{post.date}</time>
-                        <span aria-hidden='true'>&middot;</span>
-                        <span>{post.readingTime} read</span>
+                      <a href={`/posts/${post.slug}`} className='block mt-2'>
+                        <p className='text-xl font-semibold text-gray-900'>
+                          {post.title}
+                        </p>
+                        <p className='mt-3 text-base text-gray-500'>
+                          {post.description}
+                        </p>
+                      </a>
+                    </div>
+                    <div className='flex items-center mt-6'>
+                      <div className='flex-shrink-0'>
+                        <a href=''>
+                          <span className='sr-only'>{post.author}</span>
+                          <img
+                            className='w-10 h-10 rounded-full'
+                            src={urlFor(post.authImg)}
+                            alt=''
+                          />
+                        </a>
+                      </div>
+                      <div className='ml-3'>
+                        <p className='text-sm font-medium text-gray-900'>
+                          <a href='' className='hover:underline'>
+                            {post.authName}
+                          </a>
+                        </p>
+                        <div className='flex space-x-1 text-sm text-gray-500'>
+                          <time dateTime={post.datetime}>{post.date}</time>
+                          <span aria-hidden='true'>&middot;</span>
+                          <span>{post.readingTime} read</span>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export async function getStaticProps({ params }) {
   const posts = await client.fetch(homeQuery);
@@ -93,3 +106,5 @@ export async function getStaticProps({ params }) {
     },
   };
 }
+
+export default Home;
