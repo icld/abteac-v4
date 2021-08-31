083486e14447601be7d0269fd976445258a136f3
icld <ian@icldesign.com>
2021-08-31 16:11:12 -0700

    Add head and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 import SimpleNav from '../components/SimpleNav';
 import { CartProvider } from 'use-shopping-cart';
@@ -5,18 +6,28 @@ import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
-      <CartProvider
-        mode='checkout-session'
-        stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
-        currency={'usd'}
-      >
-        <SimpleNav />
-        {/* <CartSummary /> */}
-        <Component {...pageProps} />
-        <Footer />
-      </CartProvider>
-    </div>
+    <>
+      <Head>
+        <title>abteac</title>
+        <meta
+          name='description'
+          content='An amazing blog to end all of your confusion'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <CartProvider
+          mode='checkout-session'
+          stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+          currency={'usd'}
+        >
+          <SimpleNav />
+          {/* <CartSummary /> */}
+          <Component {...pageProps} />
+          <Footer />
+        </CartProvider>
+      </div>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 import { client } from '../lib/sanity/client';
 import { homeQuery } from '../lib/sanity/homeQuery';
@@ -12,87 +13,98 @@ const Home = ({ posts }) => {
   const [mappedPosts, setMappedPosts] = useState([]);
 
   return (
-    <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
-      <div className='relative top-0 h-full px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
-        <div className='absolute inset-0'>
-          <div className='bg-white h-1/3 sm:h-2/3' />
-        </div>
-        <div className='relative mx-auto max-w-7xl'>
-          <div className='text-center'>
-            <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-              A Blog to End All Confusion
-            </h2>
-            <p className='max-w-2xl mx-auto mt-3 text-xl text-gray-500 sm:mt-4'>
-              Gibberish for your achey bones
-            </p>
+    <>
+      <Head>
+        <title>abteac</title>
+        <meta
+          name='description'
+          content='An amazing blog to end all of your confusion'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <div className='relative top-0 h-full px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8'>
+          <div className='absolute inset-0'>
+            <div className='bg-white h-1/3 sm:h-2/3' />
           </div>
-          <div className='grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none'>
-            {posts.map((post) => {
-              return (
-                <div
-                  key={post.title}
-                  className='flex flex-col overflow-hidden rounded-lg shadow-lg'
-                >
-                  <div className='flex-shrink-0'>
-                    <Image
-                      src={urlFor(post.mainImage).url()}
-                      layout='responsive'
-                      width='300'
-                      height='200'
-                      className='object-cover w-full h-48'
-                      alt={post.title}
-                    />
-                  </div>
-                  <div className='flex flex-col justify-between flex-1 p-6 bg-white'>
-                    <div className='flex-1'>
-                      <p className='text-sm font-medium text-indigo-600'>
-                        <a href='' className='hover:underline'>
-                          {post.categories}
-                        </a>
-                      </p>
-                      <Link href={`/posts/${post.slug}`}>
-                        <a className='block mt-2'>
-                          <p className='text-xl font-semibold text-gray-900'>
-                            {post.title}
-                          </p>
-                          <p className='mt-3 text-base text-gray-500'>
-                            {post.description}
-                          </p>
-                        </a>
-                      </Link>
+          <div className='relative mx-auto max-w-7xl'>
+            <div className='text-center'>
+              <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+                A Blog to End All Confusion
+              </h2>
+              <p className='max-w-2xl mx-auto mt-3 text-xl text-gray-500 sm:mt-4'>
+                Gibberish for your achey bones
+              </p>
+            </div>
+            <div className='grid max-w-lg gap-5 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none'>
+              {posts.map((post) => {
+                return (
+                  <div
+                    key={post.title}
+                    className='flex flex-col overflow-hidden rounded-lg shadow-lg'
+                  >
+                    <div className='flex-shrink-0'>
+                      <img
+                        src={urlFor(post.mainImage).url()}
+                        layout='responsive'
+                        quality='40'
+                        width='300'
+                        height='200'
+                        className='object-cover w-full h-48'
+                        alt={post.title}
+                      />
                     </div>
-                    <div className='flex items-center mt-6'>
-                      <div className='flex-shrink-0'>
-                        <a href=''>
-                          <span className='sr-only'>{post.author}</span>
-                          <img
-                            className='w-10 h-10 rounded-full'
-                            src={urlFor(post.authImg)}
-                            alt=''
-                          />
-                        </a>
-                      </div>
-                      <div className='ml-3'>
-                        <p className='text-sm font-medium text-gray-900'>
+                    <div className='flex flex-col justify-between flex-1 p-6 bg-white'>
+                      <div className='flex-1'>
+                        <p className='text-sm font-medium text-indigo-600'>
                           <a href='' className='hover:underline'>
-                            {post.authName}
+                            {post.categories}
                           </a>
                         </p>
-                        <div className='flex space-x-1 text-sm text-gray-500'>
-                          <time dateTime={post.datetime}>{post.date}</time>
-                          <span aria-hidden='true'>&middot;</span>
-                          <span>{post.readingTime} read</span>
+                        <Link href={`/posts/${post.slug}`}>
+                          <a className='block mt-2'>
+                            <p className='text-xl font-semibold text-gray-900'>
+                              {post.title}
+                            </p>
+                            <p className='mt-3 text-base text-gray-500'>
+                              {post.description}
+                            </p>
+                          </a>
+                        </Link>
+                      </div>
+                      <div className='flex items-center mt-6'>
+                        <div className='flex-shrink-0'>
+                          <a href=''>
+                            <span className='sr-only'>{post.author}</span>
+                            <img
+                              className='w-10 h-10 rounded-full'
+                              src={urlFor(post.authImg)}
+                              alt=''
+                            />
+                          </a>
+                        </div>
+                        <div className='ml-3'>
+                          <p className='text-sm font-medium text-gray-900'>
+                            <a href='' className='hover:underline'>
+                              {post.authName}
+                            </a>
+                          </p>
+                          <div className='flex space-x-1 text-sm text-gray-500'>
+                            <time dateTime={post.datetime}>{post.date}</time>
+                            <span aria-hidden='true'>&middot;</span>
+                            <span>{post.readingTime} read</span>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
